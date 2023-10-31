51b5e87e1943a5865879c4b78ab91aa3e0d2dc82
“Behnekoogol” <behnekoogol@gmail.com>
2023-10-31 13:00:43 +0330

    detail_view and list_view committed

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+def list_view(request):
+    posts = Post.objects.filter(status = Post.StatusChoices.PUBLISHED,) 
+    return render(request, 'blog/list.html', {'posts':posts})
+
+
+
+def detail_view(request,year, month, day, slug):
+    post = get_object_or_404(Post, status=Post.StatusChoices.PUBLISHED, 
+                                    publish_time__year=year, 
+                                    publish_time__month=month, 
+                                    publish_time__day=day, slug=slug)
+    return render(request, 'blog/detail.html', {'post': post})
+    

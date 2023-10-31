875dccb2d67e1ae78330e10a3fbef72fe89e7b85
“Behnekoogol” <behnekoogol@gmail.com>
2023-10-31 13:01:38 +0330

    committed

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from blog.views import list_view, detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', list_view ),
+    path('detail/<int:year>/<int:month>/<int:day>/<str:slug>/', detail_view)
 ]

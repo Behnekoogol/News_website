01ec66570cc8279f61ffbfbc60c84ff4a861329d
“Behnekoogol” <behnekoogol@gmail.com>
2023-11-01 16:55:50 +0330

    updated

--- core/settings.py
+++ core/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/statics/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
 # Default primary key field type 

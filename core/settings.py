710ebdb3fe07b200862e5a777f2c4e1506bc6db2
“Behnekoogol” <behnekoogol@gmail.com>
2023-11-01 13:28:31 +0330

    update settings.py

--- core/settings.py
+++ core/settings.py
@@ -118,8 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-# Default primary key field type
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
+# Default primary key field type 
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICDILES_DIRS = [
+    Path.joinpath(BASE_DIR, './statics')
+]

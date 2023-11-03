50882f2b0d35bd3ce760df492e6b245ee2417833
“Behnekoogol” <behnekoogol@gmail.com>
2023-11-03 13:54:21 +0330

    updated views.py

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 
@@ -19,19 +19,28 @@ def detail_view(request,year, month, day, slug):
 def political(request):
     p1 = Post.objects.filter(category_id=1)
     return render(request,'political.html',{'p1':p1})
+
+
 def economy(request):
     p2 = Post.objects.filter(category_id=2)
     return render(request,'economy.html',{'p2':p2})
+
+
 def cultur(request):
     p3 = Post.objects.filter(category_id=3)
     return render(request,'cultur.html',{'p3':p3})
 
+
 def sport(request):
     p4 = Post.objects.filter(category_id=4)
     return render(request,'sport.html',{'p4':p4})
+
+
 def social(request):
     p5 = Post.objects.filter(category_id=5)
     return render(request,'social.html',{'p5':p5})
+
+
 def art(request):
     p6 = Post.objects.filter(category_id=6)
     return render(request,'art.html',{'p6':p6})

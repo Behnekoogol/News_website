26df11407886b5c2579d6b98499d831949701ef3
“Behnekoogol” <behnekoogol@gmail.com>
2023-11-03 13:53:34 +0330

    update urls for uploading media

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
 path('',views.list_view ),
@@ -9,5 +11,9 @@ path('economy.html',views.economy),
 path('social.html',views.social),
 path('art.html',views.art),
 path('cultur.html',views.cultur),
-path('<int:year>/<int:month>/<int:day>/<str:slug>/',views.detail_view)
+path('<int:year>/<int:month>/<int:day>/<str:slug>/',views.detail_view),
  ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)

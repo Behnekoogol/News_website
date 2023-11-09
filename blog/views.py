0c8bb9dc620677576afde8852f8363d6bceab0ee
“Behnekoogol” <behnekoogol@gmail.com>
2023-11-09 11:49:03 +0330

    commit for share page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import redirect, render
+from django.views import View
+
+from blog.forms import ShareForm
 from .models import Post, Category
 from django.http import Http404
 from django.shortcuts import get_object_or_404
@@ -20,65 +23,6 @@ def detail_view(request,year, month, day, slug):
     
 
 
-# def category(request, category):
-#     # Retrieve the articles for this category from your database
-#     posts = Post.objects.filter(status = Post.StatusChoices.PUBLISHED,)
-#     category = Category.objects.all()
-#     context = {
-#         'category': Category,
-#         'post': posts,
-#     }
-#     return render(request, 'category.html', context)
-
-
-
-# def politics(request):
-#     # Instead of rendering politics.html, redirect to the category view
-#     return redirect('category', category='politics')
-
-
-# def economy(request):
-#     # Instead of rendering politics.html, redirect to the category view
-#     return redirect('category', category ='economic')
-
-
-
-# def sport(request):
-#     # Instead of rendering politics.html, redirect to the category view
-#     return redirect('category', category ='sport')
-
-
-
-# def social(request):
-#     # Instead of rendering politics.html, redirect to the category view
-#     return redirect('category', category ='social')
-
-
-
-
-# def political(request):
-#     p1 = Post.objects.filter(category_id=1)
-#     return render(request,'political.html',{'p1':p1})
-# def economy(request):
-#     p2 = Post.objects.filter(category_id=2)
-#     return render(request,'economy.html',{'p2':p2})
-# def cultur(request):
-#     p3 = Post.objects.filter(category_id=3)
-#     return render(request,'cultur.html',{'p3':p3})
-
-# def sport(request):
-#     p4 = Post.objects.filter(category_id=4)
-#     return render(request,'sport.html',{'p4':p4})
-# def social(request):
-#     p5 = Post.objects.filter(category_id=5)
-#     return render(request,'social.html',{'p5':p5})
-# def art(request):
-#     p6 = Post.objects.filter(category_id=6)
-#     return render(request,'art.html',{'p6':p6})
-
-
-
-
 class CategoryListView(TemplateView):
     template_name = 'category_list.html'
 
@@ -90,3 +34,16 @@ class CategoryListView(TemplateView):
         context['category'] = category
         context['posts'] = posts
         return context
+    
+
+
+class SharePost(View):
+
+    def get(self,request, pk):
+        form = ShareForm()
+        post = get_object_or_404(Post, pk=pk)
+        return render(request, 'share.html', {'form': form, 'post': post},)
+
+
+    def post(self):
+        pass

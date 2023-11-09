e61695f8d54ca959d7c9b7d4064fcc77255d4b18
“Behnekoogol” <behnekoogol@gmail.com>
2023-11-09 11:14:35 +0330

    commit urls.py for category path

--- blog/urls.py
+++ blog/urls.py
@@ -5,13 +5,17 @@ from django.conf.urls.static import static
 
 urlpatterns=[
 path('',views.list_view ),
-path('sport.html',views.sport),
-path('political.html',views.political),
-path('economy.html',views.economy),
-path('social.html',views.social),
-path('art.html',views.art),
-path('cultur.html',views.cultur),
-path('<int:year>/<int:month>/<int:day>/<str:slug>/',views.detail_view),
+# path('sport.html',views.sport),
+# path('political.html',views.political),
+# path('economy.html',views.economy),
+# path('social.html',views.social),
+# path('art.html',views.art),
+# path('cultur.html',views.cultur),
+path('<int:year>/<int:month>/<int:day>/<str:slug>/',views.detail_view, name='detail'),
+#path('category/<str:category>/', views.category, name='category'),
+path('category/<slug:category_slug>/', views.CategoryListView.as_view(), name='category_list'),
+
+
  ]
 
 

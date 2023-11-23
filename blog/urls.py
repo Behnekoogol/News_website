6b821439e5e694c222cbfdc1b4552698af4fefcf
abbaskaf <hashemkafeshani@gmail.com>
2023-11-23 19:20:03 +0330

    changes template

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-path('',views.list_view ),
+path('',views.list_view,name='home'),
 path('newest-posts/', views.NewestPostViews.as_view(), name = 'newest-posts'),
 path('<int:year>/<int:month>/<int:day>/<str:slug>/',views.detail_view, name='detail'),
 path('category/<slug:category_slug>/', views.CategoryListView.as_view(), name='category_list'),
@@ -15,5 +15,3 @@ path('share/<int:pk>', SharePost.as_view(), name="share-post"),
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
